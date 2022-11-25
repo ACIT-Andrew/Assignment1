1f756a46dd85c12b8386b2b5b708916deb70d839
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2022-11-25 09:54:39 -0800

    Testing dark theme colors and changed button class

--- JS/script.js
+++ JS/script.js
@@ -6,5 +6,8 @@ const aside = document.querySelector('aside')
 const mainGrid = document.querySelector('#main-grid')
 const body = document.querySelector('body')
 const newDarkBtns = document.querySelectorAll('.new-dark-buttons button')
+const darkSwitch = document.querySelector('button.dark')
 const saveBtns = document.querySelectorAll('.save-buttons button')
 const textBox = document.querySelector('textarea')
+
+let changeBGco
