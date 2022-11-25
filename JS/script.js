5b0380f452ba2d9b7578faa928bf86d5dc7c761e
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2022-11-24 19:47:31 -0800

    Declare notesArray, and querySelectors

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+let notesArray = [
+    {title:"note one",body:"This is my first note." }
+]
+
+const aside = document.querySelector('aside')
+const mainGrid = document.querySelector('#main-grid')
+const body = document.querySelector('body')
+const newDarkBtns = document.querySelectorAll('.new-dark-buttons button')
+const saveBtns = document.querySelectorAll('.save-buttons button')
+const textBox = document.querySelector('textarea')
