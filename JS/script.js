17d7ffe82b93b4944332fae6d1ee8cdfb0b6811b
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2022-11-25 11:03:42 -0800

    Dark/light theme functions and EventListeners for dark theme button

--- JS/script.js
+++ JS/script.js
@@ -2,12 +2,25 @@ let notesArray = [
     {title:"note one",body:"This is my first note." }
 ]
 
+const body = document.querySelector('body')
 const aside = document.querySelector('aside')
 const mainGrid = document.querySelector('#main-grid')
-const body = document.querySelector('body')
 const newDarkBtns = document.querySelectorAll('.new-dark-buttons button')
-const darkSwitch = document.querySelector('button.dark')
+const darkSwitch = document.querySelector('button.theme')
 const saveBtns = document.querySelectorAll('.save-buttons button')
 const textBox = document.querySelector('textarea')
 
-let changeBGco
+let darkBase = (e) =>{
+    body.classList.toggle('dark')
+    aside.classList.toggle('dark')
+    mainGrid.classList.toggle('dark')
+    textBox.classList.toggle('dark')
+    darkSwitch.classList.toggle('dark')
+}
+let lightTheme = (e)=>{
+    if (darkSwitch.textContent == 'Dark Theme')darkSwitch.textContent = 'Light Theme'
+    else if (darkSwitch.textContent == 'Light Theme')darkSwitch.textContent='Dark Theme'
+}
+
+darkSwitch.addEventListener('click', darkBase)
+darkSwitch.addEventListener('click', lightTheme)
