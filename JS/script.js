b96c0f80b3f9f054d670785f10756469e7b3171f
ACIT-Andrew <115048179+ACIT-Andrew@users.noreply.github.com>
2022-11-25 15:37:53 -0800

    Hide and reset functions and EventListeners for cancel button and new note button. Started save prompt event handler.

--- JS/script.js
+++ JS/script.js
@@ -6,21 +6,49 @@ const body = document.querySelector('body')
 const aside = document.querySelector('aside')
 const mainGrid = document.querySelector('#main-grid')
 const newDarkBtns = document.querySelectorAll('.new-dark-buttons button')
+const newNote = document.querySelector('button.new')
 const darkSwitch = document.querySelector('button.theme')
-const saveBtns = document.querySelectorAll('.save-buttons button')
 const textBox = document.querySelector('textarea')
+const saveBtn = document.querySelector('button.save')
+const cancelBtn = document.querySelector('button.cancel')
 
-let darkBase = (e) =>{
+// Theme Changing
+let changeTheme = (e) =>{
     body.classList.toggle('dark')
     aside.classList.toggle('dark')
     mainGrid.classList.toggle('dark')
     textBox.classList.toggle('dark')
     darkSwitch.classList.toggle('dark')
 }
-let lightTheme = (e)=>{
-    if (darkSwitch.textContent == 'Dark Theme')darkSwitch.textContent = 'Light Theme'
-    else if (darkSwitch.textContent == 'Light Theme')darkSwitch.textContent='Dark Theme'
+let buttonText = (e)=>{
+    if (darkSwitch.textContent == 'Dark Theme'){
+        darkSwitch.textContent = 'Light Theme'
+    }
+    else if (darkSwitch.textContent == 'Light Theme'){
+        darkSwitch.textContent='Dark Theme'
+    }
 }
+darkSwitch.addEventListener('click', changeTheme)
+darkSwitch.addEventListener('click', buttonText)
 
-darkSwitch.addEventListener('click', darkBase)
-darkSwitch.addEventListener('click', lightTheme)
+// Hide and reset
+let hide = (e)=>{
+    textBox.setAttribute('hidden', '')
+    saveBtn.setAttribute('hidden', '')
+    cancelBtn.setAttribute('hidden', '')
+}
+let reset = (e)=>{
+    if (textBox.hasAttribute('hidden')){
+        textBox.value = ''
+    }
+    textBox.removeAttribute('hidden', '')
+    saveBtn.removeAttribute('hidden', '')
+    cancelBtn.removeAttribute('hidden', '')
+}
+cancelBtn.addEventListener('click', hide)
+newNote.addEventListener('click', reset)
+
+let saveLog = (e)=>{
+    prompt('Please enter a title for your new note.', '')
+}
+saveBtn.addEventListener('click', saveLog)
